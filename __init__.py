3910b2b3351a38f191b6193d6b8d31673bd47bb8
Bryan Ripley <bryan@DESKTOP-G061GRR.attlocal.net>
2024-08-06 14:37:03 -0500

    Added explanatory program __init__.py to repository

---
+++ __init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  6 14:31:59 2024
+
+@author: bryan
+"""
+
+
+'''
+In order to get the best results from the program "blue_red_plots.py": 
+1. Comment out lines 163, 164, 166, 167, 168, 175, and 176
+2. Comment out lines 181, 183, 185, 193, 194, and 195
+3. Uncomment lines 165, 169, 182, 184, 186
+4. Run the program with a csv file in mind for room monitoring and one for the TC.
+5. Notice the spacing between each x-axis label and make note of each interval for the room plot and the plot for the thermal chamber.
+6. Change the number after the percentage sign and before the two equal signs on line 94 to the number corresponding to the interval between each x-axis label for the room plot received from the compilation on step 4.
+7.Change the number after the percentage sign and before the two equal signs on line 108 to the number corresponding to the interval between each x-axis label for the thermal chamber plot received from the compilation on step 4.
+8. Uncomment out lines 163, 164, 166, 167, 168, 175, and 176
+9. Uncomment out lines 181, 183, 185, 193, 194, and 195
+10.Comment out lines 165, 169, 182, 184, 186 and then repeat step 4.
+'''
